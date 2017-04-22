4f733b6bd1c6737cb5399fc725b4b9cc296104e1
DenisKuznetsov <dkuznetsov@s-pro.io>
2017-04-22 15:41:29 +0300

    initial commit for react course, practice from 22.04.2017

---
+++ courses/react-course/src/Containers/Auth/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+	render() {
+		return (
+			<div>
+				<input type="text" placeholder="Name" />
+			</div>
+		);
+	}
+}
+
+export default Auth;
