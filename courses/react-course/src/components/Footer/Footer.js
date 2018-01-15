9d31f40d1be87d99b36371965e22a79d2b8c549f
DenisKuznetsov <dkuznetsov@s-pro.io>
2018-01-15 19:41:30 +0200

    I make something crazy

--- courses/react-course/src/components/Footer/Footer.js
+++ courses/react-course/src/components/Footer/Footer.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 import './footer.css'
 
 export default class Footer extends Component {
+    constructor() {
+        super();
+    }
     render() {
         return (
             <footer className="footer">
